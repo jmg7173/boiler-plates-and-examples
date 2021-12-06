055c3ab6de73cc3323c1c4fcd1dffcc74367fdb5
MinGyo Jung <jmg7173@gmail.com>
2021-12-07 01:44:53 +0900

    :adhesive_bandage: Fix load env location

--- api-flask/api/manage.py
+++ api-flask/api/manage.py
@@ -2,10 +2,11 @@ import os
 
 from dotenv import load_dotenv
 
+load_dotenv('.env.local')
+
 from app import create_app
 from config import get_config
 
-load_dotenv('.env.local')
 app = create_app(get_config(os.environ.get('APP_MODE')))
 
 import models
