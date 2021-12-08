12550959e325143aa33524c6ee02161bcd44d379
MinGyo Jung <jmg7173@gmail.com>
2021-12-08 21:48:10 +0900

    🚑️ Fix circular import when using manage.py
    When model is imported at api view, circular import occured.

--- api-flask/api/app.py
+++ api-flask/api/app.py
@@ -5,7 +5,6 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config, get_config
-from v1 import api as v1_api
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -14,6 +13,9 @@ migrate = Migrate()
 def create_app(config: Config) -> Flask:
     app = Flask(config.APP_NAME)
     app.config.from_object(config)
+
+    from v1 import api as v1_api
+
     app.register_blueprint(v1_api)
 
     db.init_app(app)
