630cc9ce161dc3fb777f05d82d18453d400816e3
MinGyo Jung <jmg7173@gmail.com>
2021-12-13 22:07:53 +0900

    💄 Add login view

---
+++ web-react-REST/web/src/views/auth/LoginView.style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  text-align: center;
+  width: 400px;
+  height: 450px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+  
+  h1 {
+    margin-top: 40px;
+  }
+  .ant-form {
+    text-align: left;
+    border-radius: 20px;
+    border: 2px outset black;
+    margin-top: 30px;
+    .login-form {
+      margin: 30px 40px 5px;
+      .ant-row {
+        margin-bottom: 10px;
+      }
+      .sign-in-btn {
+        color: white;
+        height: 35px;
+        font-weight: bold;
+        text-align: center;
+        background: #238636;
+        border: transparent;
+        border-radius: 10px;
+        margin-top: 40px;
+        margin-bottom: 5px;
+        transition-duration: 0.3s;
+      }
+      .sign-in-btn:hover {
+        background: #3cab51;
+      }
+    }
+  }
+`
--- web-react-REST/web/src/views/auth/LoginView.tsx
+++ web-react-REST/web/src/views/auth/LoginView.tsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { Container } from './LoginView.style'
+import { Button, Checkbox, Form, Input } from 'antd'
 
 const LoginView: React.FC = () => {
   return (
-    <>Login</>
+    <Container>
+      <h1>Sign in to Boilerplate</h1>
+      <Form
+        initialValues={{ remember: true }}
+        onFinish={(e) => {
+          const { username, password } = e
+          // FIXME: temporal code until add fetch uri
+          console.log(username, password)
+        }}
+      >
+        <div className='login-form'>
+        Username
+        <Form.Item
+          name='username'
+        >
+          <Input
+            name='Username'
+            placeholder='Username'
+          />
+        </Form.Item>
+        Password
+        <Form.Item
+          name='password'
+        >
+          <Input.Password
+            name='password'
+            placeholder='Password'
+          />
+        </Form.Item>
+        <Form.Item
+          name='remember'
+          style={{ textAlign: 'right' }}
+        >
+          <Checkbox>
+            Remember me
+          </Checkbox>
+        </Form.Item>
+        <Form.Item
+          style={{ textAlign: 'center' }}
+        >
+          <Button
+            type='primary'
+            htmlType='submit'
+            className='sign-in-btn'
+            block
+          >
+            Sign in
+          </Button>
+        </Form.Item>
+        </div>
+      </Form>
+    </Container>
   )
 }
 
