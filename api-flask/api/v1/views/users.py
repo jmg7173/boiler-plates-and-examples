2f23474c00b41cda7acdb1913ea1d245177de740
MinGyo Jung <jmg7173@gmail.com>
2021-12-25 17:49:39 +0900

    ✨ Add user profile update api

--- api-flask/api/v1/views/users.py
+++ api-flask/api/v1/views/users.py
@@ -1,10 +1,14 @@
+from typing import Union, Tuple
+
 from flask import (
     Blueprint,
     jsonify,
     request,
     Response,
 )
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
+from app import db
 from models import User
 
 users_api = Blueprint('users', __name__, url_prefix='/users')
@@ -21,3 +25,29 @@ def get_users() -> Response:
 @users_api.get('/<int:id>')
 def get_user(id: int) -> Response:
     return jsonify(User.query.get_or_404(id).to_dict())
+
+
+@users_api.put('/<int:id>')
+@jwt_required()
+def update_user(id: int) -> Union[Response, Tuple[Response, int]]:
+    username = get_jwt_identity()
+    user = User.query.filter_by(id=id).first()
+    if user.username != username:
+        return jsonify(message="Not updatable other user's profile!"), 401
+
+    data = request.get_json()
+    profile_img = data.get('profileImg')
+    if profile_img:
+        user.set_profile_img(username, profile_img)
+
+    password = data.get('password')
+    if password:
+        user.set_password(password)
+
+    email = data.get('email')
+    if email:
+        user.email = email
+
+    db.session.commit()
+
+    return jsonify(message='Successfully updated your profile!')
