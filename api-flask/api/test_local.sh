857dcfab444b7f08c944fbddec77d4d7a18ed26b
MinGyo Jung <jmg7173@gmail.com>
2022-01-02 21:58:05 +0900

    🔨 Add scripts for test on local and docker, respectively

---
+++ api-flask/api/test_local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../docker/test
+./run.sh 0
+cd ../../api
+
+IS_LOCAL=1 pytest -svv --cov=. --cov-report=term-missing
+
+docker-compose -p api-flask-test -f ../docker/dev/docker-compose.yaml down
+#rm -rf ../volumes/test
+
--- api-flask/docker/test/run.sh
+++ api-flask/docker/test/run.sh
@@ -1,17 +1,31 @@
+IS_DOCKER=$1
+
+if test -z $IS_DOCKER
+then
+  IS_DOCKER=1
+fi
+
 docker network create app-test
 docker-compose -p api-flask-test -f ../dev/docker-compose.yaml down
 docker-compose -p api-flask-test -f ../dev/docker-compose.yaml --env-file=./.env up -d
 docker-compose -p api-flask-test -f ../dev/docker-compose.yaml rm -sf nginx
 
 cnt=0
+echo "waiting for 30 seconds to DB container up"
 while ! docker-compose -p api-flask-test logs db | grep -q "ready to accept connections"; do
-  echo "waiting for db container up"
   sleep 1
   ((cnt++))
-  if [ $cnt -ge 10 ]; then
+  if [ $cnt -ge 30 ]; then
     echo "Cannot configure DB container! Tear down containers..."
     docker-compose -p api-flask-test -f ../dev/docker-compose.yaml down
     exit -1
   fi
 done
 echo "DB container up!"
+docker-compose -p api-flask-test exec api bash -c "flask db upgrade"
+
+if [ $IS_DOCKER -eq 1 ]; then
+  docker-compose -p api-flask-test exec api bash -c "pytest -svv --cov=. --cov-report=term-missing"
+  docker-compose -p api-flask-test -f ../dev/docker-compose.yaml down
+  echo "Successfully finished pytest for api!"
+fi
